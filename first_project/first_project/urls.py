dd1cbe6c5e7d590f6d4404ac6bef23fa120322cd
SlavaDef <slavadef@gmail.com>
2025-08-06 23:28:50 +0300

    5 commit

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) також підключили css
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
